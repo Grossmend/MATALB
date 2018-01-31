69063a78377b3264324e822540f72876a45819e5
Grossmend <grossmend@inbox.ru>
2018-01-31 09:01:24 +0300

    encoding_3

--- clustering/k-medoids/k_medoids.m
+++ clustering/k-medoids/k_medoids.m
@@ -1,5 +1,5 @@
 function [distrib, center, cost_present] = k_medoids(dist,k) 
-% главная функция кластеризации k-medoids тест еще тес
+% главная функция кластеризации k-medoids
 
     % кол-во вершин
     n = size(dist,1);
